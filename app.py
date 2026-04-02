eef573b945a89cf3cae7ba1761a5897f6f49c318
Adarsh-098 <ay7623614@gmail.com>
2026-04-02 12:13:08 +0530

    Updated Flask output message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Sushil Yadav roll no 61 se c cmpn c2"
+    return "Ashirwad Yadav roll no 51 se c cmpn c2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
